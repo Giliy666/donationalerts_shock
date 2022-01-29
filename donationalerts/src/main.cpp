c5244e2aa90d18d0c2bf9f5072d2f578bb477525
Giliy666 <80615459+Giliy666@users.noreply.github.com>
2022-01-29 15:03:15 +0300

    1

--- donationalerts/src/main.cpp
+++ donationalerts/src/main.cpp
@@ -7,11 +7,20 @@
 const char* ssid = "Giliy666";
 const char* password = "3216789q";
 
-const String endpoint = "https://www.donationalerts.com/widget/lastdonations?alert_type=1,4,6,8,7,10,9,3,2,5,11,12,13,14,15,16,17,18,19,20&limit=10000000000&token=6pc1ZoinN5Y1hIwcz4a7";
-const String key = "yourAPIkey";
 
-void setup() {
 
+const String endpoint = "https://www.donationalerts.com/widget/lastdonations?alert_type=1,7,10,12&limit=10000&token=6pc1ZoinN5Y1hIwcz4a7";
+const String key1 = "1,4,6,8,7,10,9,3,2,5,11,12,13,14,15,16,17,18,19,20";  //фильтр
+const String key2 = "10000000000"; //кол-во выводов
+const String key3 = "6pc1ZoinN5Y1hIwcz4a7"; //токен аккаунта
+
+const String find = "data-alert_type=\"1\"";
+
+String find2="";
+
+void setup() 
+{
+   // создаем объект для работы с HTTP
   Serial.begin(115200);
   
   // делаем небольшую задержку на открытие монитора порта
@@ -21,34 +30,46 @@ void setup() {
   WiFi.begin(ssid, password);
 
   while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
+    delay(3000);
     Serial.println("Соединяемся с Wi-Fi..");
   }
 
   Serial.println("Соединение с Wi-Fi установлено");
 }
 
-void loop() {
-  // выполняем проверку подключения к беспроводной сети
-  if ((WiFi.status() == WL_CONNECTED)) { //Check the current connection status
-    // создаем объект для работы с HTTP
+void loop() 
+{
     HTTPClient http;
-    // подключаемся к веб-странице OpenWeatherMap с указанными параметрами
-    http.begin(endpoint + key);
+    http.begin(endpoint);    
+    
     int httpCode = http.GET();  // Делаем запрос
-	
+    Serial.println(httpCode);
+
     // проверяем успешность запроса
-    if (httpCode > 0) { //Check for the returning code
-      // выводим ответ сервера
-      String payload = http.getString();
-      Serial.println(httpCode);
-      Serial.println(payload);
+    if (httpCode > 0)
+    { 
+      String payload = http.getString();  
+      
+      //Serial.println(payload);
+      int a=payload.indexOf(find);
+      int b=payload.lastIndexOf(find)+find.length();
+
+      if (a>0 && b>0)
+      {
+        Serial.println(payload.substring(a,b));  
+      }            
+      Serial.println(a);
+      Serial.println(b);
+      payload.clear();
+
     }
-    else {
+    else 
+    {
       Serial.println("Ошибка HTTP-запроса");
     }
  
     http.end(); // освобождаем ресурсы микроконтроллера
-  }
-  delay(30000);
+    
+    delay(5000);
+
 }
